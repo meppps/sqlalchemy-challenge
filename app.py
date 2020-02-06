94ef6437de699c94a9aee81ca0e7f0bffe1975c1
Mikey Epps <55078570+meppps@users.noreply.github.com>
2020-02-05 23:15:30 -0800

    Finished API

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# Flask API
+
 from flask import Flask,jsonify
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -5,7 +7,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import numpy as np
 
-
+# Create Engine/Base
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
@@ -14,14 +16,19 @@ app = Flask(__name__)
 Measurement = Base.classes.measurement
 Hawaii = Base.classes.station
 
+
+# Site map
 @app.route("/")
 def welcome():
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/api/v1.0/stations"
+        f"/api/v1.0/api/v1.0/stations<br/>"
+        f"/api/v1.0/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start>/<end>"
     )
 
+# Create session, run query, dictionary comprehenesion, jsonify
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
@@ -44,15 +51,15 @@ def tobs():
     tobQuery = session.query(Measurement.date,Measurement.tobs).filter(Measurement.date < "2017-08-23", Measurement.date > "2016-08-23").all()
     tobD = [{'date':date,'tobs':tobs} for date, tobs in tobQuery]
     return jsonify(tobD)
-    
 
-@app.route("/api/v1.0/tobs")
-def tobs():
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
     session = Session(engine)
-    tobQuery = session.query(Measurement.date,Measurement.tobs).filter(Measurement.date < "2017-08-23", Measurement.date > "2016-08-23").all()
-    tobD = [{'date':date,'tobs':tobs} for date, tobs in tobQuery]
-    return jsonify(tobD)
-
+    startEndQuery = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()[0]
+    sEQ = [{'min':startEndQuery[0],'avg':startEndQuery[1],'max':startEndQuery[2]}]
+    return jsonify(sEQ)
 
 
 if __name__ == '__main__':
